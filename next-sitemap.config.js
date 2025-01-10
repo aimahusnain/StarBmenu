17ca6562bd764823da27993605673679f833a14c
Muhammad Husnain <aimahusnain@gmail.com>
2025-01-10 09:37:06 -0800

    Devkins to Starbmenu

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://devkins.dev', // Replace with your site’s URL
+  siteUrl: 'https://starbmenu.com', // Replace with your site’s URL
   generateRobotsTxt: true, // Generate `robots.txt` file
   sitemapSize: 100, // Split sitemaps if you have a lot of pages
   changefreq: 'weekly',
