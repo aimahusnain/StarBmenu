a66d49e2a43d188e6104b821b8f31076d7e34ee3
Muhammad Husnain <aimahusnain@gmail.com>
2025-01-12 13:19:26 -0800

    Co-authored-by: Taha Amin <tahaamin2013@users.noreply.github.com>

--- src/app/(categories)/drinks/page.tsx
+++ src/app/(categories)/drinks/page.tsx
@@ -1,24 +1,13 @@
-import MainCategoryPageComponent from "@/components/Category/MainCategoryPageComponent";
-import config from "@/lib/config.json";
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: `${config.site.title} - Drinks`,
-  openGraph: {
-    title: `${config.site.title} - Drinks`,
-  },
-  icons: {
-    icon: ["/favicon.ico"],
-    apple: ["/favicon.ico"],
-    shortcut: ["favicon.ico"],
-  },
-};
+import MainCategoryPageComponent from "@/components/Category/MainCategoryPageComponent";
 
 const Drinks = () => {
   return (
-  <div>
-<MainCategoryPageComponent name="Drinks" link="/drinks" />
-  </div>);
+    <div>
+      <MainCategoryPageComponent name="Drinks" link="/drinks" />
+    </div>
+  );
 };
 
 export default Drinks;
