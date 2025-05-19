958265966116aef79b4c83bb433cff394d1002db
Muhammad Husnain <aimahusnain@gmail.com>
2025-05-19 10:48:21 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6211300056502877"
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3597647165623612"
      crossOrigin="anonymous"></script>
       </head>
       <body
